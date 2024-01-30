458d7f4756a43796add386833b9358e88994bb64
Rabillard Tony <tony.rabillard16@gmail.com>
2024-01-30 20:56:53 +0100

    Add: Jeu de données SQL

---
+++ doc/jeuDeDonnees.sql
@@ -0,0 +1,43 @@
+-- Insérer des avis clients
+INSERT INTO testimonial (
+    name, 
+    surname, 
+    message, 
+    score, 
+    garage_id)
+VALUES (
+    'Dupont', 
+    'Jean', 
+    'Très satisfait de mon expérience avec ce garage. Le service était rapide et efficace.', 
+    5, 
+    1),
+    (
+    'Martin', 
+    'Marie', 
+    'Personnel très aimable et compétent. Je recommande vivement !', 
+    4, 
+    1),
+    (
+    'Dubois', 
+    'Pierre', 
+    "Je suis déçu par la qualité du service. Les réparations n'ont pas été faites correctement.", 
+    2, 
+    1),
+    (
+    'Leroy', 
+    'Sophie', 
+    'Excellent accueil et travail impeccable. Je reviendrai sans hésiter.', 
+    5, 
+    1),
+    (
+    'Moreau', 
+    'Julien', 
+    'Service client médiocre. Les délais ont été largement dépassés et les prix étaient excessifs.', 
+    2, 
+    1),
+    (
+    'Petit', 
+    'Anne', 
+    "Je suis très satisfaite de l'intervention sur mon véhicule. Le personnel était professionnel et courtois.", 
+    5, 
+    1);
